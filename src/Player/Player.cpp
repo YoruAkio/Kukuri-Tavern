f61ff450ba07adf9765b18b9d3ef42957938a3d4
Kukuri <83948007+Mikasuru@users.noreply.github.com>
2024-10-26 16:42:17 +0700

    Update Player.cpp

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -50,6 +50,9 @@ void Player::OnConnect() {
     
     auto packet = SLoginInformationRequestPacket();
     ENetWrapper::SendPacket(this->Get(), packet);
+
+    Logger::Print(INFO, "Sent OSM {}.", GetItemManager()->GetItemsDatHash());
+    VarList::OnSuperMainStartAcceptLogon((ENetPeer*)this, GetItemManager()->GetItemsDatHash());
 }
 void Player::OnDisconnect() {
     Logger::Print(INFO, "A player disconnected with IP {}, connectId {} and {} pings.", this->GetIp(), this->GetConnectId(), this->GetPing());
